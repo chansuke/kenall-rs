fcfb5a61d4baadefdd2225eb7e5e8ca41f45ed2e
chansuke <chansuke@georepublic.de>
2021-02-18 22:43:54 +0900

    Fix documents

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,34 @@
 //!
 //! Kenall is a efficient service for searching japanese address from postal code and making
 //! complexity simple and straightforward.
-//! This is a *unofficial* [cli tool](https://github.com/chansuke/kenall-rs) of for that.
+//! This is an **unofficial** [cli tool](https://github.com/chansuke/kenall-rs) for the service.
 //!
-//! If you want to check the detail of the API, please check the page below
+//! If you want to check the detail of the API, please check the [official document](https://www.notion.so/API-47ab1a425d9e48aaad5b34b4f703c718)
 //!
-//! https://www.notion.so/API-47ab1a425d9e48aaad5b34b4f703c718
+//! # Getting Started
+//!
+//! First, you need to (signup)[https://kenall.jp/signup] to Kenall and get an API key.
+//! Once you have an API key, please write your API key into `.env` file.
+//! You can create the `.env` file by copying `.env.example` file.
+//!
+//! ```sh
+//! $ cp example.env .env
+//! $ vi .env
+//! ```
+//!
+//! # Usage
+//!
+//! Please enter valid japanese post number and you will get the address.
+//!
+//! ```sh
+//! kenall-rs 100-0001
+//! ```
+//!
+//! or
+//!
+//! ```sh
+//! kenall-rs 100-0001
+//! ```
 
 pub mod api;
 pub mod cli;
